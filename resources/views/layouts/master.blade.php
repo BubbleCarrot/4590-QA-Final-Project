3db07db8d8f88f23b912a1cd75d06eb712c80de1
ivvns <ivansomera@hotmail.com>
2022-11-26 12:42:40 -0800

    Minor changes to homepage

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -24,8 +24,11 @@
 
     @include('partials.footer')
 
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script src="{{ mix('js/app.js') }}"></script>
+ 
 </body>
 
 </html>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,32 +2,32 @@
 
 @section('content')
 <main>
-    <section id="top" class="banner items-center">
+    <section id="top" class="banner items-center xl:container m-auto px-10">
         <img src="images/banner-logo.png" alt="Logo goes here">
     </section>
-    <section class="services pb-20 pt-10">
+    <section class="services pb-20 pt-10 px-10">
         <h1 class="text-center pb-8">Our Services</h1>
-        <div class= "flex justify-center">
-            <div class="flex">
-                <div class="service-imgs px-5">
+        <div class="flex justify-center">
+            <div class="services-container flex justify-center">
+                <div class="service-imgs mb-4 mx-5">
                     <a href="#">
-                        <img class="card-1" src="images/physical.png" alt="#">
+                        <img src="images/physical.png" alt="#">
                         <p class="text-img">Physical Therapy</p>
                     </a>
                 </div>
-                <div class="service-imgs px-5">
-                    <a href="">
-                        <img class="card-2" src="images/chiro.png" alt="#">
+                <div class="service-imgs mb-4 mx-5">
+                    <a href="#">
+                        <img class="chiro" src="images/chiro.png" alt="#">
                         <p class="text-img">Chiropractor Treatment</p>
                     </a>
                 </div>
-                <div class="service-imgs px-5">
+                <div class="service-imgs mb-4 mx-5">
                     <a href="#">
-                        <img class="card-3" src="images/speech.jpg" alt="#">
+                        <img src="images/speech.jpg" alt="#">
                         <p class="text-img">Speech Therapy</p>
                     </a>
                 </div>
-                <div class="service-imgs px-5">
+                <div class="service-imgs mb-4 mx-5">
                     <a href="#">
                         <img class="card-4" src="images/occ.jpeg" alt="#">
                         <p class="text-img">Occupational Therapy</p>
@@ -38,14 +38,14 @@
     </section>
     <section class="about pb-20">
         <h2 class="text-center pb-8">Who We Are</h2>
-        <div class="container-xl flex flex-col items-center">
+        <div class="container-xl flex flex-col items-center px-10">
             <p class="pb-8">Established in 2022, Central Wellness is a physical wellness facility that connects clients with professionals that will meet their needs. Our clients are our priority, and we ensure that from the moment a client enters our facilities to the moment they leave, they feel taken care of and heard.</p>
             <p class="pb-8">Our priority is our clients' wellness and happiness.</p>
             <p>Join our loyal cliental today!</p>
         </div>
     </section>
 
-    <section class="contact">
+    <section class="contact px-10">
         <div class="flex-grow">
             <div class="max-w-screen-sm px-4 mx-auto">
 
