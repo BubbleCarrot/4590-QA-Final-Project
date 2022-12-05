c9a41b662a997102774ae4ad23e692a10c9095db
bentvandenheede <bentvand1@student.arteveldehs.be>
2022-12-05 00:31:48 -0800

    reformating section title

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 
-@section('title') {{ 'Central Wellness | Homepage' }} @endsection
+@section('title', 'Homepage | ' . config('app.name', 'Central Wellness'))
 
 @section('content')
 <main>
@@ -9,41 +9,43 @@
     </section>
     <section id="services" class="services pb-20 pt-10 px-10">
         <h2 class="text-center pb-8">Our Services</h1>
-        <div class="rectangle"></div>
-        <div class="flex justify-center">
-            <div class="services-container flex flex-wrap justify-center">
-                <div class="service-imgs mb-4 mx-5">
-                    <a href="{{ route('service','physical-therapy') }}">
-                        <img src="{{ asset('assets/images/physical.png') }}" alt="#">
-                        <p class="text-img">Physical Therapy</p>
-                    </a>
-                </div>
-                <div class="service-imgs mb-4 mx-5">
-                    <a href="{{ route('service','chiropractic-treatment') }}">
-                        <img class="chiro" src="{{ asset('assets/images/chiro.png') }}" alt="">
-                        <p class="text-img">Chiropractor Treatment</p>
-                    </a>
-                </div>
-                <div class="service-imgs mb-4 mx-5">
-                    <a href="{{ route('service','speech-therapy') }}">
-                        <img src="{{asset('assets/images/speech.jpg')}}" alt="#">
-                        <p class="text-img">Speech Therapy</p>
-                    </a>
-                </div>
-                <div class="service-imgs mb-4 mx-5">
-                    <a href="{{ route('service','occupational-therapy') }}">
-                        <img class="card-4" src="{{asset('assets/images/occ.jpeg')}}" alt="#">
-                        <p class="text-img">Occupational Therapy</p>
-                    </a>
+            <div class="rectangle"></div>
+            <div class="flex justify-center">
+                <div class="services-container flex flex-wrap justify-center">
+                    <div class="service-imgs mb-4 mx-5">
+                        <a href="{{ route('service','physical-therapy') }}">
+                            <img src="{{ asset('assets/images/physical.png') }}" alt="#">
+                            <p class="text-img">Physical Therapy</p>
+                        </a>
+                    </div>
+                    <div class="service-imgs mb-4 mx-5">
+                        <a href="{{ route('service','chiropractic-treatment') }}">
+                            <img class="chiro" src="{{ asset('assets/images/chiro.png') }}" alt="">
+                            <p class="text-img">Chiropractor Treatment</p>
+                        </a>
+                    </div>
+                    <div class="service-imgs mb-4 mx-5">
+                        <a href="{{ route('service','speech-therapy') }}">
+                            <img src="{{asset('assets/images/speech.jpg')}}" alt="#">
+                            <p class="text-img">Speech Therapy</p>
+                        </a>
+                    </div>
+                    <div class="service-imgs mb-4 mx-5">
+                        <a href="{{ route('service','occupational-therapy') }}">
+                            <img class="card-4" src="{{asset('assets/images/occ.jpeg')}}" alt="#">
+                            <p class="text-img">Occupational Therapy</p>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
     </section>
     <section id="about" class="about pb-20">
         <h2 class="text-center pb-8">Who We Are</h2>
         <div class="rectangle"></div>
         <div class="container-xl flex flex-col items-center px-10">
-            <p class="pb-8">Established in 2022, Central Wellness is a physical wellness facility that connects clients with professionals that will meet their needs. Our clients are our priority, and we ensure that from the moment a client enters our facilities to the moment they leave, they feel taken care of and heard.</p>
+            <p class="pb-8">Established in 2022, Central Wellness is a physical wellness facility that connects clients
+                with professionals that will meet their needs. Our clients are our priority, and we ensure that from the
+                moment a client enters our facilities to the moment they leave, they feel taken care of and heard.</p>
             <p class="pb-8 italic">Our priority is our clients' wellness and happiness.</p>
             <p>Join our loyal clientele today!</p>
         </div>
@@ -51,47 +53,51 @@
     <section id="contact" class="contact px-10 pb-10">
         <div class="flex-grow">
             <div class="max-w-screen-sm px-4 mx-auto">
-            <!-- Index Header -->
-            <div class="grid grid-cols-12">
-                <div class="col-span-12 flex items-center">
-                    <h2 class="flex-grow text-center">Contact Us</h3>
-                    <div class="rectangle"></div>
+                <!-- Index Header -->
+                <div class="grid grid-cols-12">
+                    <div class="col-span-12 flex items-center">
+                        <h2 class="flex-grow text-center">Contact Us</h3>
+                            <div class="rectangle"></div>
+                    </div>
+                </div>
+                <!-- End: Index Header -->
+
+                <!-- Create: Form -->
+                <div class="grid grid-cols-12 py-10">
+                    <div class="col-span-12">
+                        <form>
+                            <div class="mb-4 pb-6">
+                                <label class="block text-sm font-bold mb-2" for="">Name</label>
+                                <input class="rounded w-full py-2 px-3 text-gray-700" id="" type="name" name="name"
+                                    placeholder="full name">
+                            </div>
+
+                            <div class="mb-4 pb-6">
+                                <label class="block text-sm font-bold mb-2" for="">Email</label>
+                                <input class="rounded w-full py-2 px-3 text-gray-700" id="" type="email" name="email"
+                                    placeholder="email">
+                            </div>
+
+                            <div class="mb-4 pb-6">
+                                <label class="block text-sm font-bold mb-2" for="">Phone Number</label>
+                                <input class="rounded w-full py-2 px-3 text-gray-700" id="" type="email" name="email"
+                                    placeholder="phone number">
+                            </div>
+
+                            <div class="text-center">
+                                <button class="btn contact-btn py-2 px-4 text-white font-bol rounded-sm"
+                                    type="submit">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- End Create Form -->
                 </div>
             </div>
-            <!-- End: Index Header -->
-
-            <!-- Create: Form -->
-            <div class="grid grid-cols-12 py-10">
-                <div class="col-span-12">
-                    <form>
-                    <div class="mb-4 pb-6">
-                        <label class="block text-sm font-bold mb-2" for="">Name</label>
-                        <input class="rounded w-full py-2 px-3 text-gray-700" id="" type="name" name="name" placeholder="full name">
-                    </div> 
-                    
-                    <div class="mb-4 pb-6">
-                        <label class="block text-sm font-bold mb-2" for="" >Email</label>
-                        <input class="rounded w-full py-2 px-3 text-gray-700" id="" type="email" name="email" placeholder="email">
-                    </div>   
-
-                    <div class="mb-4 pb-6">
-                        <label class="block text-sm font-bold mb-2" for="" >Phone Number</label>
-                        <input class="rounded w-full py-2 px-3 text-gray-700" id="" type="email" name="email" placeholder="phone number">
-                    </div>   
-                                               
-                    <div class="text-center">
-                        <button class="btn contact-btn py-2 px-4 text-white font-bol rounded-sm" type="submit">Submit</button>                        
-                    </div>       
-                    </form>
-                </div>
-                <!-- End Create Form --> 
+            <div>
+                <a href="#top">
+                    <img class="arrow m-auto" src="{{asset('assets/images/arrow-up.svg')}}" alt="Page up">
+                </a>
             </div>
-        </div>
-        <div>
-            <a href="#top">
-                <img class="arrow m-auto" src="{{asset('assets/images/arrow-up.svg')}}" alt="Page up">
-            </a>
-        </div>
-    </section>       
+    </section>
 </main>
 @endsection
--- resources/views/pages/services/speech-therapy.blade.php
+++ resources/views/pages/services/speech-therapy.blade.php
@@ -1,33 +1,20 @@
 @extends('layouts.master')
 
-@section('title') {{  'Speech Therapy | Central Wellness' }} @endsection
+@section('title', 'Speech Therapy | ' . config('app.name', 'Central Wellness'))
 
 @section('content')
 <main class="services-main">
-	<h1 class="pb-8">Speech Therapy</h1>
-	<p class="services-intro">Speech Therapy focuses on the assessment and treatment of those with speech and language disorders. Our Speech Therapists help our clients develop and strengthen their speech disorders and impairments from a young age, to assist in preventing these conditions to worsen. Our methods range from oral exercises to activate the vocal cords, to jaw exercises and everything in-between.</p>
-	<section class="employees">
-		<h2 class="pb-8">Speech Therapists</h2>
-		<div class="employee-con">
-			<div class="img-con">
-				<img class="employee-img" src="{{asset('assets/images/people/person2.jpg')}}" alt="John Doe">
-			</div>
-			<div class="info-con">
-				<ul class="employee-info">
-					<li>Name: John Doe</li>
-					<li>Certifications: All of these certificates and degrees</li>
-					<li>Specialization: All of these specializations that suit what you're looking for</li>
-					<li>Days of Operation: Mondays, Tuesdays, Thursdays, and Sundays</li>
-				</ul>
-				<a href="{{ route('book.index') }}">
-					<button class="book-button">Book an Appointment</button>
-				</a>
-			</div>
-		</div>
-		<div class="employee-con">
-			<div class="img-con">
-				<img class="employee-img" src="{{asset('assets/images/people/person3.jpg')}}" alt="John Doe">
-			</div>
+    <h1 class="pb-8">Speech Therapy</h1>
+    <p class="services-intro">Speech Therapy focuses on the assessment and treatment of those with speech and language
+        disorders. Our Speech Therapists help our clients develop and strengthen their speech disorders and impairments
+        from a young age, to assist in preventing these conditions to worsen. Our methods range from oral exercises to
+        activate the vocal cords, to jaw exercises and everything in-between.</p>
+    <section class="employees">
+        <h2 class="pb-8">Speech Therapists</h2>
+        <div class="employee-con">
+            <div class="img-con">
+                <img class="employee-img" src="{{asset('assets/images/people/person2.jpg')}}" alt="John Doe">
+            </div>
             <div class="info-con">
                 <ul class="employee-info">
                     <li>Name: John Doe</li>
@@ -35,11 +22,27 @@
                     <li>Specialization: All of these specializations that suit what you're looking for</li>
                     <li>Days of Operation: Mondays, Tuesdays, Thursdays, and Sundays</li>
                 </ul>
-				<a href="{{ route('book.index') }}">
-                	<button class="book-button">Book an Appointment</button>
-				</a>
+                <a href="{{ route('book.index') }}">
+                    <button class="book-button">Book an Appointment</button>
+                </a>
             </div>
-	    </div>
-	</section>
+        </div>
+        <div class="employee-con">
+            <div class="img-con">
+                <img class="employee-img" src="{{asset('assets/images/people/person3.jpg')}}" alt="John Doe">
+            </div>
+            <div class="info-con">
+                <ul class="employee-info">
+                    <li>Name: John Doe</li>
+                    <li>Certifications: All of these certificates and degrees</li>
+                    <li>Specialization: All of these specializations that suit what you're looking for</li>
+                    <li>Days of Operation: Mondays, Tuesdays, Thursdays, and Sundays</li>
+                </ul>
+                <a href="{{ route('book.index') }}">
+                    <button class="book-button">Book an Appointment</button>
+                </a>
+            </div>
+        </div>
+    </section>
 </main>
 @endsection
